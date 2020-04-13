d981c51cd84ca740d552d56cc367c29d16dd296f
Marius Amza <mariusamza@gmail.com>
2020-04-13 20:34:16 +0300

    Removed unused function

--- src/avm_blinking.cpp
+++ src/avm_blinking.cpp
@@ -11,16 +11,3 @@ void AVM_Blink(int durata) {
 	digitalWrite(LED_BUILTIN, LOW);
 	delay(durata);
 }
-
-/*
-void AVM_Blink_Morse(char x) {
-	switch(x) {
-		case 'A':
-			AVM_Blink(250);
-			delay(250);
-			AVM_Blink(1000);
-			delay(250);
-			break;
-	}
-}
-*/
